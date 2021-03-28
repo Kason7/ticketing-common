fceb1faee54abd8dc4ed05d7c1f7b257a0d409f0
Kasper Sonne <kaspersonne27@gmail.com>
2021-03-28 12:21:34 +0100

    Update

---
+++ src/events/queueGroupNames.js
@@ -0,0 +1,9 @@
+const QueueGroupName = {
+  PAYMENT_SERVICE: 'payment-service',
+  ORDER_SERVICE: 'order-service',
+  TICKET_SERVICE: 'ticket-service',
+  EXPIRATION_SERVICE: 'expiration-service',
+  PAYMENT_SERVICE: 'payment-service',
+};
+
+exports.QueueGroupName = QueueGroupName;
--- src/events/subjects.js
+++ src/events/subjects.js
@@ -2,6 +2,9 @@ const Subject = {
   // TICKETS
   TICKET_CREATED: 'ticket:created',
   TICKET_UPDATED: 'ticket:updated',
+  // ORDERS
+  ORDER_CREATED: 'order:created',
+  ORDER_CANCELLED: 'order:cancelled',
 };
 
 exports.Subject = Subject;
